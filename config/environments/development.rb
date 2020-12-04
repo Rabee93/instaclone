61efdf39f65cf7532a19f01ad2bc5bfa9823c7f4
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-12-04 22:21:24 +0000

    dowloaded devise componants

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Debug mode disables concatenation and preprocessing of assets.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
